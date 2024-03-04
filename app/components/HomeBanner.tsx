ff0c8333345f2c6fc6d5f45eb37815a7f718690b
Elijah Lekomo <lekomoayomide@gmail.com>
2024-03-04 05:52:21 +0200

    -Completed home banner section -Added banner images

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const HomeBanner = () => {
     return ( 
         <div className="relative bg-gradient-to-r
@@ -5,12 +7,21 @@ const HomeBanner = () => {
             <div className="mx-auto px-8 py-12 flex
             flex-col gap-2 md:flex-row items-center
             justify-evenly">
-                <div>
-                    <h1>Summer Sales</h1>
-                    <p>Enjoy discount on selscted items</p>
-                    <p>GET 50% OFF</p>
+                <div className="mb-8 md:mb-0 text-center">
+                    <h1 className="text-4xl md:text-6xl
+                    font-bold text-white mb-4">Summer Sales</h1>
+                    <p className="text-lg md:text-xl text-white mb-2">Enjoy discount on selscted items</p>
+                    <p className="text-2xl md:text-5xl
+                    text-yellow-400 font-bold">GET 50% OFF</p>
                 </div>
-                <div></div>
+                <div className="w-1/3 relative aspect-video">
+                    <Image 
+                    src='/banner-image.png'
+                    fill
+                    alt="Banner Image"
+                    className="object-contain"
+                    />
+                </div>
             </div>
         </div>
      );
