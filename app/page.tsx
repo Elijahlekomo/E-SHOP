14c1a1eb8013178345815b11ca9c6619a0569656
Elijah Lekomo <lekomoayomide@gmail.com>
2024-03-06 06:18:58 +0200

    -Product card completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
+import ProductCard from "./components/products/ProductCard";
+import { products } from "./utils/products";
+import { truncateText } from "./utils/trucanteText";
 
 export default function Home() {
   return (
     <div className="p-8">
       <Container>
       <div>
-        <HomeBanner/>  
+        <HomeBanner />  
+      </div>
+      <div className="grid grid-cols-2
+      sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5
+      2xl:grid-cols-6 gap-8">
+        {products.map((product: any) => {
+        // eslint-disable-next-line react/jsx-key
+        return <ProductCard data={product}/>;
+        })}        
       </div>
       </Container>
     </div>
-  )
+  );
 }
