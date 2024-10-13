a69baf62bff0e3af0dc7dda7cac274d9c1a6ebfe
Elijah Lekomo <lekomoayomide@gmail.com>
2024-10-13 06:42:26 +0200

    Completed add product section
    Added new files AddProductForm, NullData, CategoryInput, CustomCheckInputBox, SelectColor, SelectImage, TextArea, Categories, and Colors.

---
+++ app/utils/Categories.ts
@@ -0,0 +1,33 @@
+import { AiFillPhone, AiOutlineDesktop, AiOutlineLaptop } from "react-icons/ai";
+import { MdOutlineKeyboard, MdStorefront, MdTv, MdWatch } from "react-icons/md";
+
+export const categories = [
+  {
+    label: "All",
+    icons: MdStorefront,
+  },
+  {
+    label: "Phone",
+    icons: AiFillPhone,
+  },
+  {
+    label: "Laptop",
+    icons: AiOutlineLaptop,
+  },
+  {
+    label: "Desktop",
+    icons: AiOutlineDesktop,
+  },
+  {
+    label: "Watch",
+    icons: MdWatch,
+  },
+  {
+    label: "TV",
+    icons: MdTv,
+  },
+  {
+    label: "Accesories",
+    icons: MdOutlineKeyboard,
+  },
+];
