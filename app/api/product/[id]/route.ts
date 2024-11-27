cf4800025a148522d0070e6390d60f99157476cf
Elijah Lekomo <lekomoayomide@gmail.com>
2024-11-27 11:19:36 +0200

    Commented out delete function /[id]/route.ts

--- app/api/product/[id]/route.ts
+++ app/api/product/[id]/route.ts
@@ -1,29 +1,29 @@
-import { getCurrentUser } from "@/actions/getCurrentUser";
-import { NextResponse } from "next/server";
-//import  prisma  from "@/libs/prismadb";
+// import { getCurrentUser } from "@/actions/getCurrentUser";
+// import { NextResponse } from "next/server";
+// //import  prisma  from "@/libs/prismadb";
 
-type Context = {
-  params: {
-    id: string;
-  };
-};
+// type Context = {
+//   params: {
+//     id: string;
+//   };
+// };
 
-export async function DELETE(request: Request, context: Context) {
-  const { params } = context; // Destructure params from the context
+// export async function DELETE(request: Request, context: Context) {
+//   const { params } = context; // Destructure params from the context
 
-  const currentUser = await getCurrentUser();
+//   const currentUser = await getCurrentUser();
 
-  if (!currentUser) {
-    return NextResponse.error();
-  }
+//   if (!currentUser) {
+//     return NextResponse.error();
+//   }
 
-  if (currentUser.role !== "ADMIN") {
-    return NextResponse.error();
-  }
+//   if (currentUser.role !== "ADMIN") {
+//     return NextResponse.error();
+//   }
 
-  const product = await prisma?.product.delete({
-    where: { id: params.id },
-  });
+//   const product = await prisma?.product.delete({
+//     where: { id: params.id },
+//   });
 
-  return NextResponse.json(product);
-}
+//   return NextResponse.json(product);
+// }
